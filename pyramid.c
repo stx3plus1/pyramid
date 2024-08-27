bfe6a8f37ad7e915efa4486acd49d5151b5081da
User Name <bm5580737@gmail.com>
2024-08-27 20:42:42 +0100

    pyramid reshape and credits

--- pyramid.c
+++ pyramid.c
@@ -1,8 +1,13 @@
-            int 
-	 printf (
-        const char 
-       *__restrict, 
-     ...);int main() { 
+            //
+           //\\
+          //----
+         //  code
+        // made by
+       int//cirkulx
+      /*_*/ printf (
+     const char /* */
+    *__restrict, //... 
+   ...);int main() { //
   for (int i = 20;i > 0;
  i--){ printf("%*s#%*s#\n"
 , i,"", (20 - i) * 2,"");}}
